14f2a5474e92789349a392ff1a639834db15bdc6
BasicConsideration <92383391+BasicConsideration@users.noreply.github.com>
2021-10-27 14:15:48 +0200

    Finished DatabaseService.php

--- Services/BookService.php
+++ Services/BookService.php
@@ -7,6 +7,11 @@ class BookService
 {
     public function create(array $data)
     {
+        $conn = new DatabaseService();
+        $connection = $conn->connect();
+
+
+
         return true; // Finish sequence to add book to database.
     }
 
--- Services/DatabaseService.php
+++ Services/DatabaseService.php
@@ -2,7 +2,31 @@
 
 namespace Services;
 
+use PDO;
+use PDOException;
+
 class DatabaseService
 {
+    private $host = "localhost";
+    private $dbname = "T.B.D";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function connect(): PDO
+    {
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $this->conn;
+        } catch (PDOException $e) {
+            die("Connection failed:" . $e->getMessage()); // Returns error message
+        }
+    }
+    public function disconnect(): void
+    {
+        $this->conn = null; // Closes connection
+    }
 
 }
